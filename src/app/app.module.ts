0d57fcfd4c8fb9f358498d9fee36f613ee785b64
anadoh <68846949+anadoh@users.noreply.github.com>
2022-01-26 20:27:53 +0100

    tasks service, db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskSingleComponent } from './components/task-single/task-single.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks';
 
-  constructor() { }
-  getTasks() : Task [] {
-    return TASKS
+  constructor(private http: HttpClient) {}
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
